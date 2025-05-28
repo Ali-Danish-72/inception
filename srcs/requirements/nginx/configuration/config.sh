f5ff173095d6c8c8f857b1bf29f09a49e7a205be
Ali Danish <dannyboy810110@gmail.com>
2025-05-28 13:11:26 +0400

    feature: added the adminer service
    This is bonus 4 of 5. This also contains the
    changes made to the NGINX configuration to enable
    this service.

--- srcs/requirements/nginx/configuration/config.sh
+++ srcs/requirements/nginx/configuration/config.sh
@@ -6,7 +6,8 @@ mkdir -p $KEY_DIR
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $KEY -out $CERT -subj "/C=AE/ST=Abu Dhabi/L=Abu Dhabi/O=42/OU=Dev/CN=$HOST_NAME"
 
 # Create the NGINX configuration file.
-echo "server {
+echo "# Server block for the general website
+server {
 	server_name $HOST_NAME;
 
 	listen 443 ssl http2;
@@ -24,6 +25,28 @@ echo "server {
 	location ~ \.php\$ {
 		include snippets/fastcgi-php.conf;
 		fastcgi_pass wordpress:9000;
+	}
+}
+
+# Server block for Adminer
+server {
+	server_name $HOST_NAME;
+
+	listen 8080 ssl http2;
+	ssl_certificate $CERT;
+	ssl_certificate_key $KEY;
+	ssl_protocols TLSv1.2 TLSv1.3;
+
+	root $ADMINER_ROUTE;
+	index index.php;
+
+	location / {
+		try_files \$uri \$uri/ /index.php?\$args =404;
+	}
+
+	location ~ \.php\$ {
+		include snippets/fastcgi-php.conf;
+		fastcgi_pass adminer:9000;
 	}
 }" > $NGINX_CONF;
 
